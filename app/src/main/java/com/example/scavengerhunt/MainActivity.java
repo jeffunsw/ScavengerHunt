e8049fbc2ca3275ffa4a01b79cc8098c8bfda116
Karoun <karounboghossian@Karouns-MacBook-Pro.local>
2019-03-25 12:59:56 +1100

    Another test

--- app/src/main/java/com/example/scavengerhunt/MainActivity.java
+++ app/src/main/java/com/example/scavengerhunt/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Another test
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
