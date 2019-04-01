53c234ebf3a4fbb360fb2e23ed1e7fa7c30969c7
trixiaerika <z5113234@ad.unsw.edu.au>
2019-04-01 13:56:57 +1100

    Add Reading and Maths Questions

--- app/src/main/java/com/example/scavengerhunt/ModuleQuestions.java
+++ app/src/main/java/com/example/scavengerhunt/ModuleQuestions.java
@@ -106,6 +106,33 @@ public class ModuleQuestions {
 
     public static ArrayList<ModuleQuestions> getMathsQuestions() {
         ArrayList<ModuleQuestions> mathsQuestions = new ArrayList<>();
+        mathsQuestions.add(new ModuleQuestions("MathsQ1", "Maths", "3 x 5", "8", "15", "10","15", "", false ));
+        mathsQuestions.add(new ModuleQuestions("MathsQ2", "Maths", "7 x 8", "56", "15", "54","56", "", false ));
+        mathsQuestions.add(new ModuleQuestions("MathsQ3", "Maths", "12 - 4", "16", "8", "3","8", "", false ));
+        mathsQuestions.add(new ModuleQuestions("MathsQ4", "Maths", "13 + 11", "24", "22", "26","24", "", false ));
+        mathsQuestions.add(new ModuleQuestions("MathsQ5", "Maths", "10 x 4", "14", "14", "40","40", "", false ));
+
         return mathsQuestions;
+    }
+
+    public static ArrayList<ModuleQuestions> getReadingQuestions() {
+        ArrayList<ModuleQuestions> readingQuestions = new ArrayList<>();
+        readingQuestions.add(new ModuleQuestions("ReadingQ1", "Reading", "Have you ever thought about being a pirate? You may have read about pirates in stories and thought that it would be a great life. Pirates sail all day and never have to go to school. But think again! A pirate's life may not be as much fun as you think.\n" +
+                "The writer thinks being a pirate would be ",
+                "full of problems", "fun but dangerous", "better than going to school","15", "", false ));
+        readingQuestions.add(new ModuleQuestions("ReadingQ2", "Reading", "Food is not easy to find at sea. There are no refrigerators onboard so you can forget about eating fresh food. Meat goes bad after just a few days. Dry food like flour and bread has weevils and other nasty creatures crawling inside. Imagine eating a sandwich full of insects or meat served with maggots.\n" +
+                "Weevils can be found in",
+                "flour", "meat", "fresh fruit","flour", "", false ));
+        readingQuestions.add(new ModuleQuestions("ReadingQ3", "Reading", "Pirates' hammocks are often hung over barrels of stored food. That's the perfect place for rats and mice to live. If you become a pirate you could wake up each morning sharing your bed with a few friendly rodents.\n" +
+                "The writer uses the words a few friendly rodents to ",
+                "show that only a few are good", "make an awful thing sound not so bad", "make you believe they are good pets","make an awful thing sound not so bad", "", false ));
+        readingQuestions.add(new ModuleQuestions("ReadingQ4", "Reading", "Going to the toilet is not that easy either! Over the side of the ship is the only place for this. Rough weather makes this really tricky, even for the most experienced sailor. \n " +
+                "Going to the toilet on a ship is",
+                "easy in good weather.", "difficult for all pirates.", "tricky only in bad weather.","difficult for all pirates", "", false ));
+        readingQuestions.add(new ModuleQuestions("ReadingQ5", "Reading", "It's not all plain sailing on a pirate ship. Some of the time you will have to do battle with other large, hairy pirates with long swords, huge guns and very loud cannons. This could be extremely dangerous — deadly even! \n" + "" +
+                "The words plain sailing mean pirate ships",
+                "are not easy to sail", "decorated beautifully", "can be dull and boring","are not easy to sail", "", false ));
+
+        return readingQuestions;
     }
 }
